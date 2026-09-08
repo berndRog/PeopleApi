457ce937a6f7dee93d357b169dbd599dd36a80c3
Bernd-Uwe Rogalla <bu.rogalla@icloud.com>
2026-09-08 17:07:13 +0200

    Add People count read model query

--- PeopleApi/_3_Core/People/_1_Ports/IPersonReadModel.cs
+++ PeopleApi/_3_Core/People/_1_Ports/IPersonReadModel.cs
@@ -6,6 +6,7 @@ namespace PeopleApi._3_Core.People._1_Ports;
 // Read-only queries exposed to PeopleController.
 public interface IPersonReadModel {
    Task<Result<IReadOnlyList<PersonDto>>> SelectAllAsync(CancellationToken ct);
+   Task<Result<int>> CountAsync(CancellationToken ct);
    Task<Result<PersonDto>> FindByIdAsync(Guid id, CancellationToken ct);
 }
 
@@ -14,4 +15,6 @@ public interface IPersonReadModel {
  * ----------------------
  * - Lesende Abfragen werden vom schreiborientierten Repository getrennt.
  * - Das ReadModel liefert direkt DTOs für die Application/Web-Schicht.
+ * - CountAsync ermittelt die Anzahl direkt in der Datenbank, ohne dafür alle
+ *   Person-Datensätze laden zu müssen.
  */
